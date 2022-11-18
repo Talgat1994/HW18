0afe87d4c33fd970ced3f83272e3fdbad6824267
Talgat <talidreamszzz@gmail.com>
2022-11-18 14:47:02 +0300

    save

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import "./index.css";
+
+
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [value, setValue] = useState("");
+  const postHandler = async (e) => {
+    e.preventDefault();
+    try {
+      await fetch("https://fake-api-backend.herokuapp.com/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name: value }),
+      });
+    } catch (error) {
+      return console.log("error");
+    }
+    setValue("");
+    getHandler();
+  };
+
+  const deleteHandler = async (id) => {
+    await fetch(`https://fake-api-backend.herokuapp.com/users/${id}`, {
+      method: "DELETE",
+    });
+    getHandler();
+  };
+
+  const getHandler = async () => {
+    try {
+      const promise = await fetch(
+        "https://fake-api-backend.herokuapp.com/users"
+      );
+      const result = await promise.json();
+
+      const array = result.map((item) => {
+        return { name: item.name, id: item.id };
+      });
+
+      setTodos(array);
+    } catch (error) {
+      alert("error");
+    }
+  };
+  useEffect(() => {
+    getHandler();
+  }, []);
+
+  return (
+    <div className="app">
+      <form onSubmit={postHandler}>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </form>
+      <div>
+        {todos.map((item) => {
+          console.log(todos);
+          return (
+            <div key={item.id}>
+              {item.name}
+
+              <button onClick={() => deleteHandler(item.id)}>Удалить</button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+export default App;
